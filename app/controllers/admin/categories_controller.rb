e66e89bd7f0ca5f911f46b419144230240f1fdf5
Yun Ting Hung <s9923554@gmail.com>
2018-05-20 14:49:47 +0800

    setup restaurant resources for public users and order category_id of category view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,7 +4,7 @@ class Admin::CategoriesController < Admin::BaseController
   before_action :set_category, only: [:update, :destroy]
 
   def index
-    @categories = Category.all
+    @categories = Category.order(:id)
     
     if params[:id]
       set_category
