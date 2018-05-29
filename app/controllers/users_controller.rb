80a59da36d82be0e739f60494e1f723fac9b881b
Yun Ting Hung <s9923554@gmail.com>
2018-05-29 21:32:46 +0800

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :intro, :email, :avatar)
+  end
 end
